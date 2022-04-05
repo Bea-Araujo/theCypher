491d4f4a36f78681829b08709046a16af5502a61
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-04-05 13:32:46 -0300

    now the encryption callback recognizes non-alphanumeric characters

--- scripts/index.js
+++ scripts/index.js
@@ -28,12 +28,6 @@ var displayConfigs = {
     }
 }
 
-function changeDisplay(tag, originalColor, newColor) {
-    var tag = document.querySelector(`${tag}`);
-    if (tag.classList.contains(originalColor)) { tag.classList.remove(originalColor) };
-    tag.classList.add(newColor)
-}
-
 var selectionBox = document.querySelector("#type-of-code-selector");
 
 selectionBox.addEventListener('change', function () {
@@ -59,61 +53,77 @@ var decryptionBtn = document.querySelector('#decryption-btn');
 
 encryptionBtn.addEventListener('click', function () {
     var mesage = codeInputField.value;
-
-    if (decryptionMethod == 'cc') {
-        var code = [];
-        var pace = parseInt(document.querySelector('#pace').value);
-        for (var i = 0; i < mesage.length; i++) {
-            if ((/[a-z]/).test(mesage[i])) {
-                var letterCodePaced = mesage[i].charCodeAt() + pace;
-                var item = mesage[i].charCodeAt();
-                delimitateInterval(letterCodePaced, item, code, i, pace, 97, 122);
-            } else if ((/[A-Z]/).test(mesage[i])) {
-                var letterCodePaced = mesage[i].charCodeAt() + pace;
-                var item = mesage[i].charCodeAt();
-                delimitateInterval(letterCodePaced, item, code, i, pace, 65, 90);
-            } else {
-                code.push(mesage[i])
+    if (mesage == '') {
+        alert('aaa')
+        this.checked = false;
+    } else {
+        if (decryptionMethod == 'cc') {
+            var code = [];
+            var pace = parseInt(document.querySelector('#pace').value);
+            for (var i = 0; i < mesage.length; i++) {
+                if ((/[a-z]/).test(mesage[i])) {
+                    var letterCodePaced = mesage[i].charCodeAt() + pace;
+                    var item = mesage[i].charCodeAt();
+                    delimitateInterval(letterCodePaced, item, code, i, pace, 97, 122);
+                } else if ((/[A-Z]/).test(mesage[i])) {
+                    var letterCodePaced = mesage[i].charCodeAt() + pace;
+                    var item = mesage[i].charCodeAt();
+                    delimitateInterval(letterCodePaced, item, code, i, pace, 65, 90);
+                } else if ((/\W|_/).test(mesage[i])) {
+                    code.push(mesage[i])
+                }
             }
-        }
-        code = code.join('');
+            code = code.join('');
 
-    } else if (decryptionMethod == 'base64') {
-        console.log('base64')
-        var code = btoa(mesage);
-        console.log(code)
+        } else if (decryptionMethod == 'base64') {
+            console.log('base64')
+            var code = btoa(mesage);
+            console.log(code)
+        }
+        createResultOutput(code);
     }
-    createResultOutput(code);
 })
 
 decryptionBtn.addEventListener('click', function () {
     var code = codeInputField.value;
-    if (decryptionMethod == 'cc') {
-        code = code.split(' ');
-        var pace = -parseInt(document.querySelector('#pace').value);
-        var mesage = [];
-        for (var i = 0; i < code.length; i++) {
-            if ((/[1-9]/).test(code[i])) {
-                var letter = parseInt(code[i]) + pace;
-                var item = parseInt(code[i]);
-                if (item >= 97 && item <= 122) {
-                    delimitateInterval(letter, item, mesage, i, pace, 97, 122);
-                } else if (item >= 65 && item <= 90) {
-                    delimitateInterval(letter, item, mesage, i, pace, 65, 90);
+
+    if (code == '') {
+        alert('aaa')
+        this.checked = false;
+    } else {
+        if (decryptionMethod == 'cc') {
+            code = code.split(' ');
+            var pace = -parseInt(document.querySelector('#pace').value);
+            var mesage = [];
+            for (var i = 0; i < code.length; i++) {
+                if ((/[1-9]/).test(code[i])) {
+                    var letter = parseInt(code[i]) + pace;
+                    var item = parseInt(code[i]);
+                    if (item >= 97 && item <= 122) {
+                        delimitateInterval(letter, item, mesage, i, pace, 97, 122);
+                    } else if (item >= 65 && item <= 90) {
+                        delimitateInterval(letter, item, mesage, i, pace, 65, 90);
+                    }
+                    mesage[i] = String.fromCharCode(mesage[i]);
+                } else {
+                    mesage.push(code[i])
                 }
-                mesage[i] = String.fromCharCode(mesage[i]);
-            } else {
-                mesage.push(code[i])
             }
+            mesage = mesage.join('');
+        } else if (decryptionMethod == 'base64') {
+            console.log('base64')
+            var mesage = atob(code);
+            console.log(mesage);
         }
-        mesage = mesage.join('');
-    } else if (decryptionMethod == 'base64') {
-        console.log('base64')
-        var mesage = atob(code);
-        console.log(mesage);
+        createResultOutput(mesage);
     }
-    createResultOutput(mesage);
 })
+
+function changeDisplay(tag, originalColor, newColor) {
+    var tag = document.querySelector(`${tag}`);
+    if (tag.classList.contains(originalColor)) { tag.classList.remove(originalColor) };
+    tag.classList.add(newColor)
+}
 
 function delimitateInterval(letterCodePaced, item, code, i, pace, minValue1, maxValue2) {
     if (letterCodePaced > maxValue2) {
